c1bab91d32d12cc96dea68a6b83bf0b85bbe47fe
Winston Chang <winston@stdout.org>
2012-06-10 21:10:06 -0500

    Add R source and re-roxygenize

---
+++ R/Rttf2pt1.r
@@ -0,0 +1,9 @@
+#' Rttf2pt1 package
+#'
+#' This package is a wrapper for the ttf2pt1 program, and is meant
+#' to be used with the extrafont package.
+#'
+#' @name Rttf2pt1
+#' @docType package
+#' @aliases Rttf2pt1 package-Rttf2pt1
+NULL
