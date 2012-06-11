31c708a9f393a9f920b38fbc4fe6f071b510cdbd
Winston Chang <winston@stdout.org>
2012-06-10 21:45:50 -0500

    Add utility functions

--- R/Rttf2pt1.r
+++ R/Rttf2pt1.r
@@ -7,3 +7,45 @@
 #' @docType package
 #' @aliases Rttf2pt1 package-Rttf2pt1
 NULL
+
+
+#' Returns the path to the executable for ttf2pt1
+#' @export
+which_ttf2pt1 <- function() {
+  if (.Platform$OS.type == "unix") {
+    bin <- "ttf2pt1"
+    binpath <- file.path(inst_path(), "exec", .Platform$r_arch, bin)
+  } else if (.Platform$OS.type == "windows") {
+    bin <- "ttf2pt1.exe"
+    binpath <- file.path(inst_path(), "exec", bin)
+  } else {
+    stop("Unknown platform: ", .Platform$OS.type)
+  }
+
+  # First check if it was installed with the package
+  if (file.exists(binpath))
+    return(binpath)
+
+  # If we didn't find it installed with the package, check search path
+  binpath <- Sys.which(bin)
+  if (binpath == "")
+    stop(bin, " not found in path.")
+  else
+    return(binpath)
+}
+
+
+# Borrowed this from staticdocs
+inst_path <- function() {
+  envname <- environmentName(environment(inst_path))
+
+  # If installed in package, envname == "Rttf2pt1"
+  # If loaded with load_all, envname == "package:Rttf2pt1"
+  # (This is kind of strange)
+  if (envname == "Rttf2pt1") {
+    system.file(package = "Rttf2pt1")
+  } else {
+    srcfile <- attr(attr(inst_path, "srcref"), "srcfile")
+    file.path(dirname(dirname(srcfile$filename)), "inst")
+  }
+}
